1ba053a96345fe4ac4db4da7004d1519d94c5c63
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2026-01-18 19:46:52 +0000

    feat: export all of the buttons from index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,3 @@
-// Reexport your entry components here
+export { default as GoogleSignInButton } from './components/GoogleSignInButton.svelte';
+export { default as GitHubSignInButton } from './components/GitHubSignInButton.svelte';
+export { default as XSignInButton } from './components/XSignInButton.svelte';
